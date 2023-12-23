585552799244e9de15a654fef5bf22edc36e2153
Kostiantyn Siharov <ksigaryov@gmail.com>
2023-12-23 22:57:00 +0100

    Add page not_found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 require 'pry'
 require 'erb'
+require 'rack/request'
 require './app/helpers/application_helper'
 
 module Action
@@ -9,7 +10,14 @@ module Action
     include ApplicationHelper
 
     def call(env)
-      render('home')
+      req = Rack::Request.new(env)
+      path = req.path_info
+      case path
+      when '/'
+        render('home')
+      else
+        render('not_found', status_code: 404)
+      end
     end
   end
 end
